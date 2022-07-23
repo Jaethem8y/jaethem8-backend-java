b8491ec6d971912b42e6ba079a0c77b8f4ee1ee3
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-07-23 18:44:30 +0900

    fix json issue

--- src/main/java/jaethem8/jaethem8backend/dto/blog/BlogPostDTO.java
+++ src/main/java/jaethem8/jaethem8backend/dto/blog/BlogPostDTO.java
@@ -1,19 +1,22 @@
 package jaethem8.jaethem8backend.dto.blog;
 
 import jaethem8.jaethem8backend.dto.ContentDTO;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class BlogPostDTO {
-    private final String title;
-    private final String role;
-    private final String frontend;
-    private final String backend;
-    private final String general;
-    private final List<ContentDTO> contents = new ArrayList<>();
+    private String title;
+    private String role;
+    private String frontend;
+    private String backend;
+    private String general;
+    private List<ContentDTO> contents = new ArrayList<>();
 }
